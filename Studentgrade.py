ec80fa9c8bdf1b75f84aa46538d7f79ecfabd863
Umarfarooq-Ron10 <umarron.10@gmail.com>
2025-10-06 14:09:04 +0530

    Save this file to the grade-average

--- Studentgrade.py
+++ Studentgrade.py
@@ -1,14 +1,27 @@
-marks = float(input("Enter the student's marks (0–100): "))
-if marks >= 90:
+
+print("Enter marks for 5 subjects (out of 100):")
+subject1 = float(input("Subject 1: "))  
+subject2 = float(input("Subject 2: "))  
+subject3 = float(input("Subject 3: ")) 
+subject4 = float(input("Subject 4: "))  
+subject5 = float(input("Subject 5: "))  
+
+
+total = subject1 + subject2 + subject3 + subject4 + subject5
+average = total / 5
+
+
+if average >= 90:
     grade = 'A'
-elif marks >= 75:
+elif average >= 75:
     grade = 'B'
-elif marks >= 60:
+elif average >= 60:
     grade = 'C'
-elif marks >= 50:
+elif average >= 50:
     grade = 'D'
 else:
     grade = 'Fail'
 
-
-print(f"The student's grade is: {grade}")
+print("\n--- Result ---")
+print(f"Average Marks: {average:.2f}")
+print(f"Grade: {grade}")
